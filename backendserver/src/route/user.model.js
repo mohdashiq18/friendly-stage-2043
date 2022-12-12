52acb12301e51c2ce8d9c4349a58ed4d22b7abb3
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2022-12-12 18:20:04 +0530

    sujeet

--- backendserver/src/route/user.model.js
+++ backendserver/src/route/user.model.js
@@ -1,36 +1,12 @@
 const mongoose =require("mongoose")
 
 
-/* const userSchema=new mongoose.Schema({
-    email:{type:String,require:true,unique:true},
-    password:{type:String,default:Date.now()},
-    userDetail:{
-        height:{type:Number,default:59},
-        weight:{type:Number,default:50},
-        birth:{
-            day:{type:Number,default:1},
-            month:{type:Number,default:1},
-            year:{type:Number,default:1989}
-        },
-        gender:{type:String,  enum:["Male","Female","Unknown"],default:"Unknown" },
-        units:{type:Boolean,default:true},
-    },
-    message:[String],
-    photos:[String],
-    report:[{
-        exercise_name:{type:String,default:""},
-        image:{type:String, default:""},
-        muscle_name:{type:String, default:""}
-    }]
-
-}) */
 
 const userSchema=new mongoose.Schema({
     name:{type:String,require:true},
     phone:{type:Number},
     email:{type:String,require:true,unique:true},
-    password:{type:String,default:Date.now()},
-
+    password:{type:String,require:true},
 })
 
 const User=mongoose.model("user",userSchema)
