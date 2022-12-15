337a5f82d1b7140a52b3e1eae8beb01c63571d32
Nilesh2697 <99721281+Nilesh2697@users.noreply.github.com>
2022-12-15 15:11:36 +0530

    navbar

--- beautyqueen/src/App.js
+++ beautyqueen/src/App.js
@@ -1,10 +1,15 @@
-
+import Navbar from './Components/NavBar/NavBar';
+import Drop from './Components/Dropcde/Drop';
 import './App.css';
 import AllRoutes from "./Routes/AllRoutes"
 
 function App() {
   return (
+    <div>
     <AllRoutes/>
+    <Navbar/>
+    <Drop />
+    </div>
   );
 }
 
--- beautyqueen/src/Routes/AllRoutes.jsx
+++ beautyqueen/src/Routes/AllRoutes.jsx
@@ -2,12 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import PagenotFound from "../Pages/PagenotFound"
 
 const AllRoutes = ()=>{
-    return (
-        <Routes>
-            <Route path="/" element={ <h1>Hello World</h1>} ></Route>
-            <Route path="*" element={ <PagenotFound/> } ></Route>
-        </Routes>
-    )
+//     return (
+//         <Routes>
+//             <Route path="/" element={ <h1>Hello World</h1>} ></Route>
+//             <Route path="*" element={ <PagenotFound/> } ></Route>
+//         </Routes>
+//     )
 }
 
 export default AllRoutes
