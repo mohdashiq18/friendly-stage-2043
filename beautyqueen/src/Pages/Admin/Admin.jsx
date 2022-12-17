0dc1afc26fa6bb74936e5b7ead4d97d4059cc043
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2022-12-17 14:02:07 +0530

    ready for front-end

--- beautyqueen/src/Pages/Admin/Admin.jsx
+++ beautyqueen/src/Pages/Admin/Admin.jsx
@@ -483,18 +483,18 @@ function remC(){
   }
 
   function login(){
-    axios.post("https://beautiqueenbackend.onrender.com/users/login",sign,{withCredentials:true}).then((res)=>console.log(res)).catch((e)=>console.log(e))
+    axios.post("http://localhost:8080/users/login",sign,{withCredentials:true}).then((res)=>console.log(res)).catch((e)=>console.log(e))
   }
   function logout(){
-    axios.post("https://beautiqueenbackend.onrender.com/users/logout/sujeet@gmail.com",sign,{withCredentials:true}).then((res)=>console.log(res)).catch((e)=>console.log(e))
+    axios.post("http://localhost:8080/users/logout/sujeet@gmail.com",sign,{withCredentials:true}).then((res)=>console.log(res)).catch((e)=>console.log(e))
   }
 
   function check(){
-    axios.get("https://beautiqueenbackend.onrender.com/products",{withCredentials:true}).then((res)=>console.log(res)).catch((e)=>console.log(e))
+    axios.get("http://localhost:8080/products",{withCredentials:true}).then((res)=>console.log(res)).catch((e)=>console.log(e))
   }
 
   function get(){
-    axios.get("https://beautiqueenbackend.onrender.com/users/getone",{withCredentials:true}).then((res)=>console.log(res)).catch((e)=>console.log(e))
+    axios.get("http://localhost:8080/users/getone",{withCredentials:true}).then((res)=>console.log(res)).catch((e)=>console.log(e))
   }
 
   /* async function make(){
