52d52b75d583ebf785c04e54b2a919353c29f236
Rahulzhp <akumari42872@gmail.com>
2022-12-18 17:29:11 +0530

    z

--- beautyqueen/src/App.js
+++ beautyqueen/src/App.js
@@ -1,32 +1,11 @@
 import "./App.css";
-<<<<<<< HEAD
-import Drop from "./Components/Dropcde/Drop";
-import Footer from "./Components/Footer/Footer";
-import Navbar from "./Components/NavBar/NavBar";
-
-=======
-import Login from "./Components/Login/Login";
-
-import SingUp from "./Components/SignUp/SingUp";
-import content from "./content";
-import Cart from "./Pages/Home/Cart";
->>>>>>> e1285ebac7ad8876521325b6bd0dd297c2c5c481
 import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
   return (
-<<<<<<< HEAD
+
     <div className="App">
-    {/* <Navbar/>
-    <Drop/> */}
-     <AllRoutes />
-    {/* <Footer/> */}
-=======
-    <div>
-    
-     <AllRoutes />
-     {/* <Cart></Cart> */}
->>>>>>> e1285ebac7ad8876521325b6bd0dd297c2c5c481
+    <AllRoutes/>
     </div>
   );
 }
